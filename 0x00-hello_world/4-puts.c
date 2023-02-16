bfd9fe43686f5c028beb435b82425ed20b3ad31c
Mbuguacodes254 <lizziecodes254@gmail.com>
2023-02-16 12:57:30 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include<stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int min(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
