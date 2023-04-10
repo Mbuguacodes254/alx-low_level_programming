1d3d1284270c7972d61c4c8c797954161d76f811
Mbuguacodes254 <lizziecodes254@gmail.com>
2023-04-10 13:31:47 -0700

    White Lion

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+/*
+ * White Lion
+ * Auth: Mbugua Wairimu
+ */
+
+#include "main.h"
+
+/**
+ * print_binary - Prints binary rep
+ * @n: number to print.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
